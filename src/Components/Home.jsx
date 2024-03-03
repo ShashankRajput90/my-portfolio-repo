a0e85704204912f93f6c84d7999a53e1838d6424
Shashank Lodhi <76483015+ShashankRajput90@users.noreply.github.com>
2024-03-03 15:17:12 +0000

    Changed the images.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/src/images/IMG_8663_1.JPG";
+import image from "../images/src/images/my-image.jpg";
 
 const imageAltText = "Nothing just me and my ordinary photo.";
 
