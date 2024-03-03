81550c06b74886ad1d76a189a966eef23f30afcb
Shashank Lodhi <76483015+ShashankRajput90@users.noreply.github.com>
2024-03-03 16:31:59 +0000

    Added custom images and text to the portfolio page ```

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/src/images/my-image.jpg";
+import image from "../../src/images/my-image.jpg";
 
 const imageAltText = "Nothing just me and my ordinary photo.";
 
