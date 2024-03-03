fbe83d7c2ca688a4813a30a769228c72871876a2
Shashank Lodhi <76483015+ShashankRajput90@users.noreply.github.com>
2024-03-03 14:42:54 +0000

    Added a new image and modified the Home component ```

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Shashank Lodhi",
+  title: "Student & Graphic Designer",
+  email: "rajputshashank9012@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
-  twitter: "microsoft",
+  twitter: "",
   youTube: "Code",
 };
 
